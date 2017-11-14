085c48ab8b37e96166a3d6cdcbde3bcaf8a6fabf
Steve Todd <steve.todd7@gmail.com>
2017-11-14 15:56:24 -0500

    Configured DB Context and connection string

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -3,6 +3,8 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ElevenNote.Data
 {
@@ -29,5 +31,34 @@ namespace ElevenNote.Data
         {
             return new ElevenNoteDbContext();
         }
+
+        // Added special
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+
     }
+
+    // Added special
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    // Added special
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.RoleId);
+        }
+    }
+
 }
