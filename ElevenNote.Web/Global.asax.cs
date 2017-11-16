1409cca0297fa3120d6d197a1dde5714debf3188
Steve Todd <steve.todd7@gmail.com>
2017-11-16 16:46:46 -0500

    Added WebApi

--- ElevenNote.Web/Global.asax.cs
+++ ElevenNote.Web/Global.asax.cs
@@ -14,6 +14,7 @@ namespace ElevenNote.Web
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            WebApiConfig.register();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
